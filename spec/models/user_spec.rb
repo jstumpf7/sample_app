cdf2c153987bb95f5e249416f0748ec69cd3e683
Jonathan Stumpf <jonathan.stumpf@alum.lehigh.edu>
2014-03-29 15:48:04 -0400

    added an authetication system for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,51 @@ require 'spec_helper'
 describe User do
 
 	before do
-	     @user = User.new(name: "Example User", email: "user@example.com") 
+	     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") 
 	end
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
+
+      describe "with a password that's too short" do
+       before { @user.password = @user.password_confirmation = "a" * 5 }
+      end
+
+      describe "return value of authenticate method" do
+       before { @user.save }
+       let(:found_user) { User.find_by(email: @user.email) }
+      
+	describe "with valid password" do
+	 it { should eq found_user.authenticate(@user.password) }
+	end
+
+	describe "with invalid password" do
+	 let (:user_for_invalid_password) { found_user.authenticate("invalid") }
+	 it { should_not eq user_for_invalid_password }
+	 specify { expect(user_for_invalid_password).to be_false }
+	end
+      end
+ 
+
+	describe "when password is not present" do
+	 before do
+	  @user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+	 end
+	 it { should_not be_valid }
+	end
+
+	describe "when password doesn't match confirmation" do
+	 before { @user.password_confirmation = "mismatch" }
+	 it { should_not be_valid }
+	end
 
 	describe "when name is not present" do
 	 before { @user.name = " " }
@@ -35,10 +71,10 @@ describe User do
 	     @user.email = invalid_address
 	     expect(@user).not_to be_valid
 	   end
-	  end
 	 end
+        end
 
-	 describe "when email is valid"
+	 describe "when email is valid" do
 	  it "should be valid" do
 	    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
 	    addresses.each do |valid_address|
@@ -47,5 +83,15 @@ describe User do
 	    end
 	  end
 	 end
+
+	describe "when the email address should be unique" do
+	 before do
+	  user_with_same_email = @user.dup
+	  user_with_same_email.email = @user.email.upcase
+	  user_with_same_email.save
+	 end
+
+	 it { should_not be_valid }
+	end
 end
 
