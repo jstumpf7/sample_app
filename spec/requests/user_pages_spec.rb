89aa6e0404b3ead75c072b5e3eea9790b5f545fa
Jonathan Stumpf <jonathan.stumpf@alum.lehigh.edu>
2014-04-08 21:26:56 -0400

    sign-up update

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,4 +12,12 @@ subject { page }
     it {should have_title(full_title('Sign Up')) }
 
   end
+  
+  describe "profile page" do
+	let(:user) { FactoryGirl.create(:user) }
+	before { visit user_path(user) }
+	
+	it {should have_content(user.name)}
+	it {should have_title(user.name)}
+  end
 end
