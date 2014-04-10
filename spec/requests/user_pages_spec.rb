e52d6c0428427ce489fcc837aace0edafdf6f336
Jonathan Stumpf <jonathan.stumpf@alum.lehigh.edu>
2014-04-09 22:03:06 -0400

    sign up update

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,7 @@ describe "UserPages" do
 
 subject { page }
 
+
   describe "signup page" do
 
     before { visit signup_path }
@@ -11,6 +12,27 @@ subject { page }
     it {should have_content('Sign up') }
     it {should have_title(full_title('Sign Up')) }
 
+	let(:submit) { "Create my account" }
+	
+	describe "with invalid information" do
+		it "should not create a user" do
+			expect { click_on submit }.not_to change(User, :count)
+		end
+	end
+	
+	describe "with valid information" do
+		before do
+			fill_in "Name",			with: "Jonathan Stumpf"
+			fill_in "Email",		with: "jstumpf@gmail.com"
+			fill_in "Password",		with: "yes"
+			fill_in "Confirmation", with: "yes"
+		end
+		it "should create a user" do
+			expect do
+				click button submit
+			end.to change(User, :count).by(1)
+		end
+	end
   end
   
   describe "profile page" do
